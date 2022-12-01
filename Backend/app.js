426ab6ecc5382e496c54c669b213a7ad82f3ac79
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2022-12-01 21:54:31 +0530

    get and post applied

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const app = express();
 
+//cors refers to the method that allows you to make requests to the server deployed at a different domain
+//cross origin resourse sharing  -- CORS
+
+const cors = require('cors');
+app.use(cors());   
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -32,6 +37,7 @@ app.post("/",(req,res)=>{
     const newTask = new Task({
         name:req.body.name
     })
+   
     newTask.save(function(err,result){
         if (err){
             console.log(err);

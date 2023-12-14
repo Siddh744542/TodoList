ae83bd83aa904144a42423c656ea18e6b1c8c306
Siddharth Tiwari <tiwarisiddharth27@gmail.com>
2023-12-14 14:34:35 +0530

    mongodb server added

---
+++ server/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+
+//cors refers to the method that allows you to make requests to the server deployed at a different domain
+//cross origin resourse sharing  -- CORS
+
+const cors = require('cors');
+app.use(cors());   
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}todolist.dn4cl3x.mongodb.net/?retryWrites=true&w=majority`);
+
+const taskSchema = {
+    name : {
+        type: String,
+        required:[true,"no item specified"]
+    }
+};
+  
+const Task =  mongoose.model("Task", taskSchema);
+  
+app.get("/",(req,res)=>{
+    Task.find(function(err,tasks){
+        if(err){
+            console.log(err);
+        } else {
+            res.send(tasks);
+        }
+    });
+});
+
+app.delete("/",(req,res)=>{
+    console.log(req.body.idd);
+    Task.deleteOne({_id:req.body.idd},function(err){
+        if(err)
+           console.log(err);
+        else
+           res.send("deleted successfully");
+    })
+})
+
+app.post("/",(req,res)=>{
+    const newTask = new Task({
+        name:req.body.name
+    })
+    newTask.save(function(err,result){
+        if (err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
+})
+
+app.listen(7000,()=>{
+    console.log("Server has started succesfully");
+})
